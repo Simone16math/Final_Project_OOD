0b26501faa1520b03a23d48ef92502876eb80be7
iiitsanni <annikamaringer@online.de>
2025-11-17 23:13:10 -0500

    starting the project i guess

--- FinalProject/src/MenuFactory.java
+++ FinalProject/src/MenuFactory.java
@@ -1,12 +1,13 @@
 public class MenuFactory {
     public Menu createItem(String type) {
-        if(type.equalsIgnoreCase("IceCream")){
+        if (type.equalsIgnoreCase("IceCream")) {
             return new IceCream();
-        }else if(type.equalsIgnoreCase("Cookie")){
+        } else if (type.equalsIgnoreCase("Cookie")) {
             return new Cookie();
-        }else if(type.equalsIgnoreCase("Drinks")){
+        } else if (type.equalsIgnoreCase("Drinks")) {
             return new Drinks();
+        } else {
+            return null;
         }
-    }
 
-}
+    }}
