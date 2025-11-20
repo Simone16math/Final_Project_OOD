1274d965de4b4e27c50b7da96f4d45f5c20c0a4d
Simone16math <s.charles26@ncf.edu>
2025-11-20 12:31:02 -0500

    adding another decorator to test out things

---
+++ FinalProject/src/ChocolateChips.java
@@ -0,0 +1,17 @@
+public class ChocolateChips extends ToppingDecorator{
+
+    public ChocolateChips(IceCream1 iceCream1){
+        super(iceCream1);
+    }
+
+    @Override
+    public String getDescription(){
+        return super.getDescription() + ", Chocolate Chips";
+    }
+
+    @Override
+    public double getPrice(){
+        return super.iceCream1.getPrice() + 0.50;
+    }
+
+}
