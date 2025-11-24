0c944fa7e4501eac39f8fef2242117bfd8002771
Simone16math <s.charles26@ncf.edu>
2025-11-24 12:41:11 -0500

    Changed price() from void to double

--- FinalProject/src/Sprinkles.java
+++ FinalProject/src/Sprinkles.java
@@ -8,7 +8,8 @@ public class Sprinkles extends ToppingDecorator{
         return super.getDescription() + ", Sprinkles";
     }
 
-    public void price(){
+    public double price(){
         // need to change the price so that it updates or add price as an attribute to icecream
+        return iceCream.price() + 0.25;
     }
 }
