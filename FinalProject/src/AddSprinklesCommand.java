419355d79f8dc5df35c03932b30ddd71fbd441d9
Simone16math <s.charles26@ncf.edu>
2025-11-24 13:32:46 -0500

    Attempting to complete Command related things

---
+++ FinalProject/src/AddSprinklesCommand.java
@@ -0,0 +1,13 @@
+public class AddSprinklesCommand {
+    private BasicIceCream1 basicIceCream1;
+
+    public AddSprinklesCommand(BasicIceCream1 basicIceCream1){
+        this.basicIceCream1 = basicIceCream1;
+    }
+
+    @Override
+    public void execute(){
+
+    }
+
+}
