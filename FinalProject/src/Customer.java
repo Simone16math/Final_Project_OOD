ce776bf5dd685f4d6de69127eee87a053d7bdea2
iiitsanni <annikamaringer@online.de>
2025-11-29 16:36:20 -0500

    added the Observer pattern

--- FinalProject/src/Customer.java
+++ FinalProject/src/Customer.java
@@ -1,5 +1,34 @@
-public class Customer implements Observer {
-    public String name;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Customer implements Subject {
+    //List to register customer
+    public ArrayList<Observer> observers = new ArrayList<Observer>();
+
+    public Customer() {
+        //constructor
+        observers = new ArrayList<>();
+    }
+    // Method to add an observer
+    @Override
+    public void registerObserver(Observer o) {
+        observers.add(o);
+    }
+    // Method to remove an observer
+    @Override
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+
+    }
+    // Method to notify an observer
+    @Override
+    public void notifyObservers(String Order) {
+        for (Observer o : observers) {
+            o.update(Order);
+        }
+
+    }
+   /* public String name;
 
     public String getName() {
         return name;
@@ -15,5 +44,5 @@ public class Customer implements Observer {
     }
     public void notification(){
         System.out.println("You ordered: " );
-    }
+    }*/
 }
