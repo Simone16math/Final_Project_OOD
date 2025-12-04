013af60753f98500d15fc394dc7e635e84150f81
Simone16math <s.charles26@ncf.edu>
2025-12-04 01:19:15 -0500

    updated the add commands

--- FinalProject/src/AddChocolateSauceCommand.java
+++ FinalProject/src/AddChocolateSauceCommand.java
@@ -14,6 +14,7 @@ public class AddChocolateSauceCommand implements Command{
 
     @Override
     public void execute(){
+        iceCream = new ChocolateSauceDecoratorIceCream(iceCream);
 
     }
 
--- FinalProject/src/AddSprinklesCommand.java
+++ FinalProject/src/AddSprinklesCommand.java
@@ -17,7 +17,6 @@ public class AddSprinklesCommand implements Command{
     public void execute(){
         // adds sprinkles to the iceCream
         iceCream = new SprinklesDecoratorIceCream(iceCream);
-
         System.out.println(iceCream.getDescription());
 
 
