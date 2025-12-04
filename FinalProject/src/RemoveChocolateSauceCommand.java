b57621058f95e3e09b23bd7d3d8786ae9aea56e4
Simone16math <s.charles26@ncf.edu>
2025-12-03 20:51:35 -0500

    wanted to implement an undo feature if you want to remove toppings to the ice cream (or at least implement it)

---
+++ FinalProject/src/RemoveChocolateSauceCommand.java
@@ -0,0 +1,13 @@
+public class RemoveChocolateSauceCommand implements Command{
+
+    private IceCream iceCream;
+
+    public RemoveChocolateSauceCommand(IceCream iceCream){
+        this.iceCream = iceCream;
+    }
+
+    @Override
+    public void execute(){
+
+    }
+}
